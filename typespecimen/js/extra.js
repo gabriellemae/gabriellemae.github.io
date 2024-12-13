50befb43e354c454fe501821aa83c632a49c8bb8
gabriellemae <ggrimm@sva.edu>
2024-12-12 20:02:57 -0500

    adjusting Rapunzel and Brother placement

--- typespecimen/js/extra.js
+++ typespecimen/js/extra.js
@@ -12,4 +12,17 @@ splitter.addEventListener('mousedown', (e) => {
   });
 });
 
+console.log('Hello');
+
+//set up the element that triggers my function
+let box = document.querySelector('.box');
+let glyphs = document.querySelector('.glyph');
+
+//set up the event listener for clicking on the box
+glyph.addEventListener('click', function(){
+  //what happens on click
+  console.log('a note to yourself');
+
+  box.classList.toggle('bookmark');
+});
 
