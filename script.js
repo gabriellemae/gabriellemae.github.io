6ad6763ecfeead9cea1a1bc18c1b755d080b9e64
gabriellemae <ggrimm@sva.edu>
2024-12-14 00:04:36 -0500

    Color and styling

--- script.js
+++ script.js
@@ -35,3 +35,17 @@ document.addEventListener('mouseup', () => {
     isDragging = false;
     circle.style.cursor = 'pointer';
 });
+
+//set up the element that triggers my function
+let trigger = document.getElementById('button');
+
+//set up the event listener for clicking on the trigger
+trigger.addEventListener('click', function(){
+    //what happens on click
+    console.log('darkmode button clicked!');
+    document.body.classList.toggle('darkmode');
+});
+
+// function functionName(){
+
+// }
